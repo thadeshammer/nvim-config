614ddb195cd06f57b46ca9064158f3c48eaea8de
Steve Hannum <shannum@pixelforensics.com>
2026-06-12 16:12:34 -0400

    fix xml spacing/tab width

--- lua/thades/tabs_and_spaces.lua
+++ lua/thades/tabs_and_spaces.lua
@@ -23,9 +23,9 @@ vim.api.nvim_create_autocmd("FileType", {
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = "xml",
 	callback = function()
-		vim.opt_local.tabstop = 8
-		vim.opt_local.shiftwidth = 8
-		vim.opt_local.softtabstop = 8
+		vim.opt_local.tabstop = 2
+		vim.opt_local.shiftwidth = 2
+		vim.opt_local.softtabstop = 2
 		vim.opt_local.expandtab = true
 	end,
 })
