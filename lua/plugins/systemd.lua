e4b84736debc11b79863be2bf672763bf08b265a
Steve Hannum <shannum@pixelforensics.com>
2026-06-26 09:30:42 -0400

    remove old unused plugin

--- lua/plugins/systemd.lua
+++ lua/plugins/systemd.lua
@@ -6,19 +6,21 @@ return {
   },
 
   {
-    "jose-elias-alvarez/null-ls.nvim",
-    ft = { "systemd" },
-    dependencies = { "nvim-lua/plenary.nvim" },
-    opts = function()
-      local null_ls = require("null-ls")
-      return {
-        sources = {
-          null_ls.builtins.diagnostics.systemd, -- calls `systemd-analyze verify`
-        },
-      }
-    end,
-    config = function(_, opts)
-      require("null-ls").setup(opts)
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      local lint = require("lint")
+
+      -- Register systemd-analyze for systemd filetypes
+      lint.linters_by_ft = lint.linters_by_ft or {}
+      lint.linters_by_ft.systemd = { "systemd-analyze" }
+
+      -- Automatically run the linter on save or leave insert mode
+      vim.api.nvim_create_autocmd({ "BufWritePost", "InsertLeave" }, {
+        callback = function()
+          lint.try_lint()
+        end,
+      })
     end,
   },
 }
