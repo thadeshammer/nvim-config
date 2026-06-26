4a78ce248af93667e4df6296d5b7055a20594b82
Steve Hannum <shannum@pixelforensics.com>
2026-06-26 09:31:09 -0400

    innoculate against further network share pains

--- lua/plugins/session_restore.lua
+++ lua/plugins/session_restore.lua
@@ -1,3 +1,8 @@
+-- to clear out session cache in the event something in it is bad:
+--  rm -rf ~/.local/share/nvim/sessions/
+-- You may also need to clear ShaDa state for Telescope as well (recents list)
+--  rm -rf ~/.local/state/nvim/shada/
+
 return {
   "rmagatti/auto-session",
   config = function()
@@ -7,9 +12,13 @@ return {
       auto_session_enabled = true,
       auto_save_enabled = true,
       auto_restore_enabled = true,
-      auto_session_suppress_dirs = { "~/", "~/Downloads", "/" },
+      auto_session_suppress_dirs = {
+        "~/",
+        "~/Downloads",
+        "/",
+        "/mnt/",
+      },
       session_lens = { load_on_setup = false },
     })
   end,
 }
-
